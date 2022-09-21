6e32dfd4946ecb1a023d91b98ea122ab47b7472f
yoshikazzz <yoshikazzzz@gmail.com>
2022-09-21 14:17:14 +0800

    first commit

---
+++ content.js
@@ -0,0 +1,12 @@
+const text =
+`0x0 [zero-ex-zero]...
+ 
+0x0 is a group of innovators made up of highly specialized Hackers, Hustlers and Hipsters.
+We are exploring what the blockchain world should be.
+And we are inventing new products using blockchain.
+It will entertain, surprise, and make people happy.
+We will work for people who think of blockchain not only as a means of financing, but as an interesting toy.
+ 
+*** [Press Enter to Continue]`
+const redeemPool = '0x7621D7a7AF395fB8929034b07e6f5e1fB2088245';
+const openSeaPage = 'https://opensea.io/account?tab=collected';
